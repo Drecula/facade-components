81a82410433f6c986423154a0a0af556624f837a
Andre Lockhart <alockhart@flexgen.com>
2024-08-18 16:14:16 -0400

    added comment

--- src/components/Latest/Button.tsx
+++ src/components/Latest/Button.tsx
@@ -3,7 +3,9 @@ import withFacade from '../../HOC/withFacade'
 import OldButton, { ButtonProps as OldButtonProps } from '../Legacy/Button.tsx'
 import NewButton, { ButtonProps as NewButtonProps } from '../Latest/Base/Button.tsx'
 
-// Button HOC, the button that users will consume
+// Button HOC, the button that users will consume. It works for making the right button (see App.tsx)
+// It has a bug, since isNew is runtime value, ts has to do props union vs assigning them explicitly
+// Props union screws up code complete since it can't tell which props for a button
 const Button = withFacade<OldButtonProps, NewButtonProps>(
   'Button',
   OldButton,
